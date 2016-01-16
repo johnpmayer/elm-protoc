0b9b97a1e81beb71c963e589511a6f734c8fc168
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2016-01-16 15:50:20 -0500

    native decoders and encoders wired up

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,15 +5,21 @@ module Lib
 import Prelude hiding                             (readFile, putStr)
     
 import Data.ByteString.Lazy                       (ByteString, readFile, putStr)
-import Data.Maybe                                 (fromJust)
+import Data.Foldable                              (toList)
+import Data.Maybe                                 (fromMaybe)
+import Data.Text                                  (Text)
+import                                            qualified Data.Text as T
 import Data.Text.Lazy                             (fromStrict, toStrict)
 import Data.Text.Lazy.Encoding                    (decodeUtf8, encodeUtf8)
 import System.IO                                  (FilePath)
-import Text.DescriptorProtos.FileDescriptorProto  (FileDescriptorProto, package)
+import Text.DescriptorProtos.DescriptorProto      (DescriptorProto)
+import                                            qualified Text.DescriptorProtos.DescriptorProto as D
+import Text.DescriptorProtos.FileDescriptorProto  (FileDescriptorProto)
+import                                            qualified Text.DescriptorProtos.FileDescriptorProto as FD
 import Text.ProtocolBuffers.Basic                 (utf8)
 import Text.ProtocolBuffers.ProtoCompile.Parser   (parseProto)
 
-import Templates                                  (nativeModule)
+import Templates
 
 parseProtoFile :: FilePath -> IO ()
 parseProtoFile filename = 
@@ -25,12 +31,37 @@ parseProtoFile filename =
         do
           putStrLn (show fileDescriptor)
           putStr $ genNativeModule protoContents fileDescriptor
-          
+
+messageTypename :: DescriptorProto -> Text
+messageTypename = toStrict . decodeUtf8 . utf8 . fromMaybe (error "Didn't find a name for the descriptor") . D.name
 
 genNativeModule :: ByteString -> FileDescriptorProto -> ByteString
 genNativeModule protoContents fileDescriptor = 
   let
-    packagename = toStrict . decodeUtf8 . utf8 . fromJust $ package fileDescriptor
-    modulename = packagename
+    packagename :: Text
+    packagename = toStrict . decodeUtf8 . utf8 . fromMaybe (error "Did not find a package name in the .proto file") $ FD.package fileDescriptor
+    descriptors :: [DescriptorProto]
+    descriptors = toList $ FD.message_type fileDescriptor
+    encoders :: Text
+    encoders = T.concat $ do
+      descriptor <- descriptors
+      let typename = messageTypename descriptor
+      return . encodeNative $ typename
+    decoders :: Text
+    decoders = T.concat $ do
+      descriptor <- descriptors
+      let typename = messageTypename descriptor
+      return . decodeNative $ typename
+    values :: Text
+    values = T.concat [decoders, encoders]
+    exports :: Text
+    exports = T.concat $ do
+      descriptor <- toList descriptors
+      let typename = messageTypename descriptor
+      prefix <- ["encode", "decode"]
+      return . nativeModuleExport $ T.concat [T.pack prefix, typename]
+    modulename :: Text
+    modulename = T.toTitle packagename
+    protoSource :: Text
     protoSource = toStrict . decodeUtf8 $ protoContents
-  in encodeUtf8 . fromStrict $ nativeModule packagename modulename protoSource
+  in encodeUtf8 . fromStrict $ nativeModule packagename modulename protoSource values exports
--- src/Templates.hs
+++ src/Templates.hs
@@ -5,23 +5,58 @@ module Templates where
 import Data.Text                (Text)
 import NeatInterpolation        (text)
 
--- TODO - should really support a list 
-nativeModule :: Text -> Text -> Text -> Text
-nativeModule packagename modulename protoSource =
+nativeModule packagename modulename protoSource moduleValues moduleExports =
   [text|
-    Elm.Native.$modulename = Elm.Native.$modulename || {}
-    Elm.Native.$modulename = function(_elm) {
+    Elm.Native.${modulename} = Elm.Native.${modulename} || {}
+    Elm.Native.${modulename} = function(_elm) {
       "use strict";
-      _elm.Native.$modulename = _elm.Native.$modulename || {}
-      if (_elm.Native.$modulename.values) {
-        return _elm.Native.$modulename.values
+      _elm.Native.${modulename} = _elm.Native.${modulename} || {}
+      if (_elm.Native.${modulename}.values) {
+        return _elm.Native.${modulename}.values
       }
       
       // .proto source
-      var protoSource = `$protoSource`;
+      var protoSource = `${protoSource}`;
       
       var ProtoBuilder = dcodeIO.ProtoBuf.loadProto(protoSource);
-      var Proto = ProtoBuilder.build("$packagename");
+      var Proto = ProtoBuilder.build("${packagename}");
+
+      ${moduleValues}
+
+      return _elm.Native.${modulename}.values = {
+        ${moduleExports}
+      }
     }
   |]
-  
+
+nativeModuleExport valuename =
+  [text|
+        ${valuename}: ${valuename},
+  |]
+
+decodeNative typename = 
+  [text|
+    var decode${typename} = function(blob) {
+      return Proto.${typename}.decode(blob);
+    }
+  |]
+
+decodeElm modulename typename = 
+  [text|
+    decode${typename} : Buffer -> ${typename}Contract
+    decode${typename} = Native.${modulename}.decode${typename}
+  |]
+
+encodeNative typename = 
+  [text|
+    var encode${typename} = function(message_${typename}} {
+      return message_${typename}.toArrayBuffer();
+    }
+  |]
+
+encodeElm modulename typename =
+  [text|
+    encode${typename} : ${typename}Contract -> Buffer
+    encode${typename} = Native.${modulename}.encode${typename}
+  |]
+
